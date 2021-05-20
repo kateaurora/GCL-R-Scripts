9b48c488f8448ac0e4957241b4e9c334341f1222
erondeau <erondeau@uvic.ca>
2021-05-20 10:58:37 -0700

    Change version of ggforce to dev to fix plotting error

--- plot_baseline_eval_summary.GCL.r
+++ plot_baseline_eval_summary.GCL.r
@@ -50,12 +50,19 @@ plot_baseline_eval_summary.GCL <- function(summary, file, method = c("MCMC", "PB
   
   if(!require("pacman")) install.packages("pacman"); library(pacman); pacman::p_load(tidyverse) #Install packages, if not in library and then load them.
   
-  if(!require("ggforce")) install.packages("ggforce"); library(ggforce) #Install packages, if not in library and then load them.
+  if(!require("ggforce")) install.packages("devtools"); library(devtools); devtools::install_github("crotoc/ggforce"); library(ggforce) #Install packages, if not in library and then load them.
   
   # Check methods
   method_check <- summary$estimates$method %>% 
     unique() %>% 
     sort()
+  
+  
+
+  
+  
+  
+  
   
   if(method=="both"&!sum(method_check==sort(c("PB", "MCMC")))==2){
     
@@ -156,12 +163,12 @@ plot_baseline_eval_summary.GCL <- function(summary, file, method = c("MCMC", "PB
       ggplot2::geom_abline(intercept = 0.1, slope = 1, lty = 2) +
       ggplot2::geom_abline(intercept = -0.1, slope = 1, lty = 2) +
       ggplot2::scale_colour_manual(name = "Reporting Group", values = group_colors) +
-      ggplot2::geom_text(aes(x = .3, y = 1, label = paste0("RMSE:", round(RMSE, digits = 3))), color = "black", size = 2.8)+ 
-      ggplot2::geom_text(aes(x = .3, y = .94, label = paste0("Bias:", round(Mean_Bias, digits = 3))), color="black", size = 2.8)+
-      ggplot2::geom_text(aes(x = .3, y = .88, label = paste0("90% Within: ", round(100*`90%_within`, 1), "%")), color = "black", size = 2.8)+
-      ggplot2::geom_text(aes(x = .3, y = .82, label = paste0("Within Interval: ", round(100*Within_Interval, 1), "%")), color = "black", size = 2.8)+
+      ggplot2::geom_text(aes(x = .3, y = 1, label = paste0("RMSE:", round(RMSE, digits = 3))), color = "black", size = 2.8) + 
+      ggplot2::geom_text(aes(x = .3, y = .94, label = paste0("Bias:", round(Mean_Bias, digits = 3))), color="black", size = 2.8) +
+      ggplot2::geom_text(aes(x = .3, y = .88, label = paste0("90% Within: ", round(100*`90%_within`, 1), "%")), color = "black", size = 2.8) +
+      ggplot2::geom_text(aes(x = .3, y = .82, label = paste0("Within Interval: ", round(100*Within_Interval, 1), "%")), color = "black", size = 2.8) +
       facet_wrap_paginate(~ test_group,ncol=3,nrow=3,page=i) +
-      ggplot2::theme(legend.position = "none", strip.text.x = element_text(size = 16), panel.spacing.y = unit(3, "lines"))+
+      ggplot2::theme(legend.position = "none", strip.text.x = element_text(size = 16), panel.spacing.y = unit(3, "lines")) +
       ggplot2::xlab("True Proportion") +
       ggplot2::ylab("Posterior Mean Reporting Group Proportion") +
       ggtitle(paste0("Baseline evaluation test results: ", meth), subtitle = paste0(summary$estimates$total_samps %>% unique(), " sample mixtures"))
